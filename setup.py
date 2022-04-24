dcde59c0ccb38ddd83d5f5197413d733abb5222c
Peter Krahulik <peter.krahulik@gmail.com>
2022-04-24 14:31:37 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+"""
+Decorator providing capability to emit and read a dataclass as a binary buffer.
+"""
+from setuptools import setup
+
+setup(
+    name='dataclass_struct',
+    version='0.9',
+    license='MIT',
+    author="Peter Krahulik",
+    author_email='peter.krahulik@googlemail.com',
+    py_modules=["dataclass_struct"],
+    url='https://https://github.com/Thinkeye/dataclass_struct',
+    keywords='dataclass, struct'
+)
