096ee87a33bdcf573f03e7b5ec5c40969923c7a3
Peter Krahulik <peter.krahulik@gmail.com>
2022-04-24 15:46:01 +0200

    add description to package

--- setup.py
+++ setup.py
@@ -3,13 +3,24 @@ Decorator providing capability to emit and read a dataclass as a binary buffer.
 """
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    readme_description = fh.read()
+
 setup(
     name='dataclass_struct',
-    version='0.9',
+    description='Decorator for writing and reading the dataclass as binary buffer using struct.',
+    long_description=readme_description,
+    long_description_content_type="text/markdown",
+    version='0.9.2',
     license='MIT',
     author="Peter Krahulik",
     author_email='peter.krahulik@googlemail.com',
     py_modules=["dataclass_struct"],
     url='https://https://github.com/Thinkeye/dataclass_struct',
-    keywords='dataclass, struct'
+    keywords='dataclass, struct',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
